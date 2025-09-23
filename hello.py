0faa24583588a0e1c98bb609d370aa51b28a758d
sailalithkanumuri8 <saikanbolt@gmail.com>
2025-09-22 23:40:49 -0400

    Initial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/clubs', methods=['GET'])
+def get_clubs():
+    df = pd.read_csv('sample_clubs.csv')
+    return jsonify(df.to_dict('records'))
+
+@app.route('/clubs/<club_name>', methods=['GET'])
+def get_club_by_name(club_name):
+    df = pd.read_csv('sample_clubs.csv')
+    club_data = df[df['club_name'].str.lower() == club_name.lower()]
+    return jsonify(club_data.to_dict('records'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
